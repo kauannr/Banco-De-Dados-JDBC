44066ae8ce7c694f545acf5a5db73df968779f6f
Kauan Ferreira <kfr@academico.ufpb.br>
2024-02-01 17:19:42 -0300

    Atualizando dados do Banco

---
+++ Atualizar Dados/src/App.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import DB.DB;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Connection conn = null;
+        PreparedStatement st = null;
+        try {
+            conn = DB.getconnection();
+            st = conn.prepareStatement("UPDATE seller "
+                    + "SET BaseSalary = BaseSalary + ? "
+                    + "WHERE "
+                    + "(DepartmentId = ?)");
+
+            st.setDouble(1, 200.0);
+            st.setInt(2, 2);
+            int linhasAfetadas = st.executeUpdate();
+            System.out.println("Sucesso! Linhas afetadas: " + linhasAfetadas);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            DB.closeStatemet(st);
+            DB.closeConnection();
+        }
+    }
+}
